04fb57a6bd1391e1811cafd2b0c45fdb0578939c
Alda Latifah <aldaa.ltfh@gmail.com>
2023-03-07 23:37:55 +0800

    first commit

---
+++ app/src/main/java/com/example/butraapplication/ui/SplashScreen.kt
@@ -0,0 +1,30 @@
+package com.example.butraapplication.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.example.butraapplication.R
+import com.example.butraapplication.ui.main.MainActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, second.toLong())
+    }
+    companion object{
+        const val second = 3000
+    }
+}
